2cd9db08732e661ba34f24ce967565fbad71dbf0
Vlad Matveev <matveev_vlad@me.com>
2020-03-09 17:07:12 +0300

    Рефакторинг запуска приложения

--- hw02-annotationConfig/src/main/java/me/decentos/Main.java
+++ hw02-annotationConfig/src/main/java/me/decentos/Main.java
@@ -1,7 +1,6 @@
 package me.decentos;
 
 import me.decentos.config.AppConfig;
-import me.decentos.service.CsvService;
 import me.decentos.service.QuestionsService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -10,9 +9,7 @@ public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 
-        CsvService csvService = context.getBean(CsvService.class);
         QuestionsService questionsService = context.getBean(QuestionsService.class);
-
         questionsService.run();
     }
 }
