d2379642149e5fc2f5fb466094ec2f16f654bbd3
Vlad Matveev <matveev_vlad@me.com>
2020-03-15 09:44:41 +0300

    Fix CommandLineRunner

--- hw04-springShellApp/src/main/java/me/decentos/shell/ApplicationStartupRunner.java
+++ hw04-springShellApp/src/main/java/me/decentos/shell/ApplicationStartupRunner.java
@@ -3,12 +3,15 @@ package me.decentos.shell;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.MessageSource;
+import org.springframework.core.annotation.Order;
+import org.springframework.shell.jline.InteractiveShellApplicationRunner;
 import org.springframework.stereotype.Component;
 
 import java.util.Locale;
 
 @Component
 @RequiredArgsConstructor
+@Order(InteractiveShellApplicationRunner.PRECEDENCE - 1000)
 public class ApplicationStartupRunner implements CommandLineRunner {
 
     private final MessageSource messageSource;
