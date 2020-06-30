9861aae4996f423b4ef21723e5f83819ee7c81f7
Vlad Matveev <matveev_vlad@me.com>
2020-06-30 13:14:19 +0300

    Homework completed

--- hw16-springActuator/src/main/java/me/decentos/SpringActuatorApp.java
+++ hw16-springActuator/src/main/java/me/decentos/SpringActuatorApp.java
@@ -2,7 +2,9 @@ package me.decentos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @SpringBootApplication
 public class SpringActuatorApp {
     public static void main(String[] args) {
